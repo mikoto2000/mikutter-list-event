8d19ecca2d4a24e340c5ef26efc90bda9d762342
mikoto2000 <mikoto2000@gmail.com>
2014-08-06 01:47:01 +0900

    Revert "list プラグインへ依存を無くした。"
    うん、勘違いだった...。
    
    This reverts commit b47f96f67930f97984b98fcff941c29c7a5b3531.

--- list-event.rb
+++ list-event.rb
@@ -2,29 +2,15 @@
 
 Plugin.create(:list_event) do
     defevent :list_update,  priority: :routine_passive, prototype: [UserList, Message]
-    def user_lists
-        @user_lists ||= []
-    end
-
-    # 起動時に全てのサービスを舐めてリストを登録していく。
-    on_boot do |_|
-        Service.instances.each { |service|
-            (service.lists).next do |lists|
-                lists.each { |list|
-                    user_lists.push(list)
-                }
-            end
-        }
-    end
 
     # リストのツイート受信毎に list_upate イベントを発行する
     on_appear do |messages|
         messages.each{ |message|
-            for list in user_lists do
-                if list.member?(message.user)
+            Plugin[:list].timelines.each{ |slug, list|
+                if list.related?(message)
                     Plugin.call(:list_update, list, message)
                 end
-            end
+            }
         }
     end
 end
